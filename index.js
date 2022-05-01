69ffba56b7f1731977e2c8dd65bf028b8d3aad9e
mollikacomputer <mollikacomputer3@gmail.com>
2022-05-01 23:59:15 +0600

    product data connect to mongodb database and show client side data from mongodb database

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-const products = [
-    {id:1, name: "Oil-5 liters can", quantity: 10, supplier: "Rajesh ", description:"Product Descripton 1", price: 850, picture: "https://www.product-reviews.net/wp-content/uploads/2019/08/biscuits.jpg"},
-    {id:2, name: "rice", quantity: 21, supplier: "Mahesh ", price: 950, description:"Product Descripton 6", picture: "https://5.imimg.com/data5/HQ/SB/MY-19588085/sunflower-oil-5ltr-500x500.png"},
-    {id:3, name: "biscuites liters can", quantity: 11, supplier: "Bijoy ", description:"Product Descripton 2", price: 550, picture: "https://5.imimg.com/data5/HQ/SB/MY-19588085/sunflower-oil-5ltr-500x500.png"},
-    {id:4, name: "Oil-4 liters can", quantity: 15, supplier: "Tinku ", description:"Product Descripton 3", price: 650, picture: "https://5.imimg.com/data5/HQ/SB/MY-19588085/sunflower-oil-5ltr-500x500.png"},
-    {id:5, name: "Oil-3 liters can", quantity: 18, supplier: "Pintu ", description:"Product Descripton 4", price: 350, picture: "https://www.product-reviews.net/wp-content/uploads/2019/08/biscuits.jpg"},
-    {id:6, name: "Oil-2 liters can", quantity: 19, supplier: "Sujit ", description:"Product Descripton 5", price: 250, picture: "https://5.imimg.com/data5/HQ/SB/MY-19588085/sunflower-oil-5ltr-500x500.png"}
-]
+const uri = "mongodb+srv://dbuser1:S7QV4BHrOQ981beK@cluster0.pgbec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db("asignment11").collection("product");
+        app.get('/products', async(req, res)=>{
+            const query ={};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        } )
+        // post product add a new product
+        app.post('/product', async(req, res) => {
+            const newProduct = req.body;
+            console.log('adding new product', newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
 
-// database connection code from mongodb
+    }finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
-app.get('/product/:id', (req, res) =>{
-    console.log(req.params);
-    const id = parseInt(req.params.id);
-    const product = products.find(u => u.id === id );
-    res.send(product);
-})
-// product data add to server side post data
-app.post('/product', (req, res) =>{
-    console.log('request', req.body);
-    res.send('post method success');
-} )
+
+
+
+
+
 app.get('/', (req, res) =>{
     res.send('Connected with my server assignment qqq11sss test')
-});
-app.get('/products', (req, res)=>{
-    res.send(products);
 });
 
 app.listen(port, ()=>{
